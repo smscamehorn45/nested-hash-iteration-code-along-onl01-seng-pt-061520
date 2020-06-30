b8884be04c90ad2fe79c0e51d11ee823094b40a7
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-30 02:00:55 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,5 +14,12 @@ contacts = {
  
   
 def remove_strawberry(contacts)
+contacts.each do |person, contact_details_hash|\
+
 contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      
+end
+end
 end
